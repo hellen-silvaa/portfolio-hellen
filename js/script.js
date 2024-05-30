daaef2f7514bbd648957bad3cea054b58df9c8e0
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-30 12:52:24 -0300

    fazendo requisição

--- js/script.js
+++ js/script.js
@@ -35,4 +35,28 @@ displayError(){
     this.form.innerHTML = this.settings.error;
 
 }
+sendForm(){
+    fetch(this.url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: "",
+    });
 }
+
+init(){
+    if(this.form) this.formButton.addEventListener("click", () => this.displaySucces());
+    return this;
+}
+}
+
+const ForSubmit= new ForSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='sucess'> Mensagem enviada!</h1>",
+    error: "<h1 class='error'> Não foi possível enviar sua mensagem. </h1>",
+
+});
+ForSubmit.init();
